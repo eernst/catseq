732ac17c6ab70f055fce76117c6f8b3a01bbe5b4
Evan Ernst <ernst.evan@gmail.com>
2014-01-29 00:56:00 -0500

    command line arg parsing basics

--- gnomics.go
+++ gnomics.go
@@ -1,10 +1,24 @@
 package main
 
+import "os"
 import "fmt"
+import "flag"
+
+
 //import "code.google.com/p/biogo"
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+
+	flag.Usage = func() {
+	    fmt.Fprintf(os.Stderr, "usage: %s [options] scaffolds.fasta\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
 	
+	flagVerbosePtr := flag.Bool("v", false, "verbose")
+	flag.Parse()
 	
+	if (*flagVerbosePtr) {
+		fmt.Println("verbose:", *flagVerbosePtr)
+	}
+//	fmt.Println("Args from flag.Args:", flag.Args())
 }
